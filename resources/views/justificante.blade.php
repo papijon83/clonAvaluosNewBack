ba6145ce4f050b8aa8e3281e5399a959e37c7bed
iovidal33 <iovidal33@gmail.com>
2021-02-24 14:44:43 -0600

    PDF NEW CAT v18

--- resources/views/justificante.blade.php
+++ resources/views/justificante.blade.php
@@ -159,7 +159,7 @@
                             </td>
                         </tr>
                         <tr>
-                            <td><b>INMUEBLE QUE VALÚA Y NO EVALÚA:</b></td>
+                            <td><b>INMUEBLE QUE VALÚA:</b></td>
                             <td>
                             @isset($infoAvaluo['Sociedad_Participa']['inmuebleQueSeValua'])
                                 <span class="grises">{{$infoAvaluo['Sociedad_Participa']['inmuebleQueSeValua']}}</span>
