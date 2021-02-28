dd202de4505c1f7033fb6d255908330ebaf8c5f3
jpalacig <papijon83@gmail.com>
2021-02-28 13:20:45 -0600

    modificacion

--- app/Models/ReimpresionNuevo.php
+++ app/Models/ReimpresionNuevo.php
@@ -2103,7 +2103,7 @@ class ReimpresionNuevo
 
         $infoReimpresion['Sociedad_Participa']['Propietario'] = array();
         $infoReimpresion['Sociedad_Participa']['Propietario']['Tipo_persona'] = isset($arrPropietario['A.Paterno']) && !is_array($arrPropietario['A.Paterno']) ? "Física" : "Moral";
-        error_log(json_encode($arrPropietario));
+        Log::info(json_encode($arrPropietario));
         if(isset($arrPropietario['A.Paterno']) && !is_array($arrPropietario['A.Paterno'])){
             $infoReimpresion['Sociedad_Participa']['Propietario']['Nombre'] = $arrPropietario['Nombre']." ".$arrPropietario['A.Materno']." ".isset($arrPropietario['A.Materno']) && !is_array($arrPropietario['A.Materno']) ? $arrPropietario['A.Materno'] : '';
         }else{
